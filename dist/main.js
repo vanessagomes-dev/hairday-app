e0ddb13fd5cec9b762f807e79e289c0bb13c9935
Vanessa Gomes <vanessagomesdev@gmail.com>
2025-11-12 19:26:27 -0300

    build(babel): adiciona e configura Babel para compatibilidade com navegadores antigos — Instalado o Babel e integrado ao Webpack, permitindo a transpilação do código JavaScript para versões compatíveis com navegadores mais antigos.

--- dist/main.js
+++ dist/main.js
@@ -146,7 +146,7 @@ eval("{module.exports = __webpack_require__.p + \"3ed99243f708a649d7dc.svg\";\n\
   \*********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_global_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles/global.css */ \"./src/styles/global.css\");\n/* harmony import */ var _styles_form_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./styles/form.css */ \"./src/styles/form.css\");\n/* harmony import */ var _styles_schedule_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./styles/schedule.css */ \"./src/styles/schedule.css\");\n\r\n\r\n;\r\n\r\n\n\n//# sourceURL=webpack://hairday/./src/main.js?\n}");
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_global_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles/global.css */ \"./src/styles/global.css\");\n/* harmony import */ var _styles_form_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./styles/form.css */ \"./src/styles/form.css\");\n/* harmony import */ var _styles_schedule_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./styles/schedule.css */ \"./src/styles/schedule.css\");\n\n\n\n\n\n\n//# sourceURL=webpack://hairday/./src/main.js?\n}");
 
 /***/ }),
 
