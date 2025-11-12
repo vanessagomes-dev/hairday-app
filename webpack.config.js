8f3f9a33f637fd2dcf19c3b196e522a1a5775825
Vanessa Gomes <vanessagomesdev@gmail.com>
2025-11-12 19:14:28 -0300

    build(assets): adiciona plugin para copiar ícones durante o processo de build — Instalado e configurado plugin no webpack.config.js para copiar automaticamente os ícones da pasta de origem para a pasta de destino, garantindo que sejam incluídos na build final da aplicação.

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,47 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    target: "web",
-    mode: "development",
+  target: "web",
+  mode: "development",
 
-    entry: path.resolve(__dirname, 'src', 'main.js'),
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, 'dist')
+  entry: path.resolve(__dirname, "src", "main.js"),
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
     },
+    port: 3000,
+    open: true,
+    liveReload: true,
+  },
 
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist')
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+      favicon: path.resolve(__dirname, "src", "assets", "scissors.svg"),
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets"),
+          to: path.resolve(__dirname, "dist", "src", "assets"),
         },
-        port: 3000,
-        open: true,
-        liveReload: true
-    },
+      ],
+    }),
+  ],
 
-    plugins: [new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, 'index.html'),
-        favicon: path.resolve(__dirname, 'src', 'assets', 'scissors.svg')
-    })
-],
-
-module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-        ],
-},
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
