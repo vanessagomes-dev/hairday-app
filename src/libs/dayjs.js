a756e1f2a770248c4b51623f201ef88de6516102
Vanessa Gomes <vanessagomesdev@gmail.com>
2025-11-12 19:45:30 -0300

    feat(date): adiciona biblioteca Day.js para manipulação de datas — Instalado o pacote day.js e criado arquivo de configuração na pasta libs para centralizar o uso e personalização de funções relacionadas a data e hora na aplicação.

---
+++ src/libs/dayjs.js
@@ -0,0 +1,5 @@
+import dayjs from 'dayjs';
+import "dayjs/locale/pt-br"; 
+
+// Configura o Day.js para usar o idioma português do Brasil
+dayjs.locale('pt-br');
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 "use strict"
 
+import "./libs/dayjs.js"
+
 import "./styles/global.css"
 import "./styles/form.css"
 import "./styles/schedule.css"
+
