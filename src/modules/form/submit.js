5413f854a31f28ad97f056bc9b8783f031f465ad
Vanessa Gomes <vanessagomesdev@gmail.com>
2025-11-13 16:21:08 -0300

    feat(schedule): prepara e valida dados antes do envio para a API de agendamento — Implementada validação dos campos obrigatórios para garantir o preenchimento correto antes do envio. Adicionada lógica para inserir o horário na data selecionada e gerar um identificador único para cada agendamento, prevenindo erros e garantindo integridade dos dados enviados à API.

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,17 +1,51 @@
-import dayjs from "dayjs"
+import dayjs from "dayjs";
 
-
-const form = document.querySelector("form")
-const selectedDate = document.getElementById("date")
-const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
+const form = document.querySelector("form");
+const clientName = document.getElementById("client");
+const selectedDate = document.getElementById("date");
+const inputToday = dayjs(new Date()).format("YYYY-MM-DD");
 
 //Carrega data atual e defini a data min
-selectedDate.value = inputToday
-selectedDate.min = inputToday
+selectedDate.value = inputToday;
+selectedDate.min = inputToday;
 
 //Evento de submit do formulário.
 form.onsubmit = (event) => {
-    event.preventDefault()
+  event.preventDefault();
 
-    console.log("Formulário enviado!")
-}
+  try {
+    // recuperando o nome do cliente.
+    const name = clientName.value.trim();
+
+    if (!name) {
+      return alert("Por favor, insira o nome do cliente!");
+    }
+
+    // Recupera o horario selecionado.
+    const hourSelected = document.querySelector(".hour-selected");
+
+    if (!hourSelected) {
+      return alert("Por favor, selecione um horário!");
+    }
+
+    // Recuperar somente a hora.
+    const [hour] = hourSelected.innerText.split(":");
+
+    // Insere a hora na data.
+    const when = dayjs(selectedDate.value).add(hour, "hour");
+    
+    // gera um ID
+    const id = new Date().getTime();
+
+    console.log({
+        id,
+        name,
+        when,
+    })
+
+
+  } catch (error) {
+    alert("Não é possível realizar o agendamento");
+    console.log(error);
+  }
+};
