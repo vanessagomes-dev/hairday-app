af83ff0ca8d639a22bdb859d36c0640332c60fde
Vanessa Gomes <vanessagomesdev@gmail.com>
2025-11-14 17:48:32 -0300

    testes

--- src/services/schedule-cancel.js
+++ src/services/schedule-cancel.js
@@ -5,8 +5,9 @@ export async function scheduleCancel({ id }) {
     const response = await fetch(`${apiConfig.baseURL}/schedules/${id}`, {
       method: "DELETE",
     });
-    if (!response.ok) throw new Error("Falha ao deletar");
+ 
     alert("Agendamento cancelado com sucesso!");
+
   } catch (error) {
     console.log(error);
     alert("Não foi possível cancelar o agendamento.");
